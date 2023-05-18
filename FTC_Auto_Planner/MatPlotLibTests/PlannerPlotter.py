5535e7bb9643788c4b92c8bba47abbc674263e29
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-05-18 14:46:05 -0400

    Changes buttons with Tkinter

--- FTC_Auto_Planner/MatPlotLibTests/PlannerPlotter.py
+++ FTC_Auto_Planner/MatPlotLibTests/PlannerPlotter.py
@@ -4,42 +4,27 @@ from matplotlib.widgets import RadioButtons
 from matplotlib.backend_bases import MouseButton
 
 fig, ax = plt.subplots()
-fig.subplots_adjust(left=0.3)
-
-axcolor = 'lightgoldenrodyellow'
-rax = fig.add_axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(rax, ('Start', 'End'))
+img = plt.imread(r"C:\Users\vihaa\FTC-Autonomous-Planner\FTC_Auto_Planner\MatPlotLibTests\background.jpg")
+ax.imshow(img, extent=[-10, 10, -10, 10])
 
 class Main:
-    startposition = False
-    endposition = False
-
     x_left_vals = []
     y_left_vals = []
 
     x_right_vals = []
     y_right_vals = []
 
-    def radiofunc(self, label):
-        radiodict = {'Start': self.startposition, 'End': self.endposition}
-        typeposition = radiodict[label]
-
-        if typeposition == self.startposition:
-            self.startposition = True
-            self.endposition = False
-        else:
-            self.startposition = False
-            self.endposition = True
-
     def onclick(self, event):
         print("in")
         xdata, ydata = event.xdata, event.ydata
         buttonpressed = event.button
+        print(buttonpressed)
+        print(event)
         if not (xdata == None):
-            if (self.startposition and (buttonpressed == MouseButton.LEFT)):
+            if (buttonpressed == MouseButton.LEFT):
                 self.x_left_vals.append(xdata)
                 self.y_left_vals.append(ydata)
-            elif (self.endposition and (buttonpressed == MouseButton.RIGHT)):
+            elif (buttonpressed == MouseButton.RIGHT):
                 self.x_right_vals.append(xdata)
                 self.y_right_vals.append(ydata)
 
@@ -48,7 +33,7 @@ class Main:
 
             fig.canvas.draw()
 callback = Main()
-radio.on_clicked(callback.radiofunc)
+
 plt.connect('button_press_event',callback.onclick)
 
 plt.show()
